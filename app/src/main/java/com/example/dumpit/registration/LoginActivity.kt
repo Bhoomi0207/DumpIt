746787c608d7089afcf5c56dad94f6f4537ba36c
Bhoomi0207 <bhoomiag0207@gmail.com>
2023-11-27 19:44:15 +0530

    Home

--- app/src/main/java/com/example/dumpit/registration/LoginActivity.kt
+++ app/src/main/java/com/example/dumpit/registration/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.dumpit.registration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dumpit.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState:Bundle?) {
