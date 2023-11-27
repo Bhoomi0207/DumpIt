55dfcccf021cf401e72c8510c7367048e7e9836c
Siddharth Yadav <ykunal422@gmail.com>
2023-11-27 19:43:14 +0530

    here we go

--- app/src/main/java/com/example/dumpit/registration/LoginActivity.kt
+++ app/src/main/java/com/example/dumpit/registration/LoginActivity.kt
@@ -1,11 +1,78 @@
 package com.example.dumpit.registration
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import androidx.appcompat.widget.AppCompatButton
+import com.example.dumpit.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState:Bundle?) {
+
+    private lateinit var usernameEditText: MaterialAutoCompleteTextView
+    private lateinit var passwordEditText: MaterialAutoCompleteTextView
+    private lateinit var loginButton: AppCompatButton
+    private lateinit var createAccountTextView: TextView
+    private lateinit var auth: FirebaseAuth // Firebase Authentication instance
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+
+        // Initialize views
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+        createAccountTextView = findViewById(R.id.createAccountTextView)
+
+        // Set click listeners
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Validate username and password (add your own validation logic)
+            if (isValidCredentials(username, password)) {
+                // Authenticate with Firebase
+                signInWithEmailAndPassword(username, password)
+            } else {
+                showToast("Invalid credentials. Please try again.")
+            }
+        }
+
+        createAccountTextView.setOnClickListener {
+            // Handle create account text view click
+            // Navigate to the registration or sign-up activity
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        // Add your validation logic here
+        return username.isNotEmpty() && password.isNotEmpty()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    showToast("Login successful")
+                   // val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish() // Finish LoginActivity to prevent going back to it using the back button
+                } else {
+                    showToast("Authentication failed. Please check your credentials.")
+                }
+            }
     }
 }
--- app/src/main/java/com/example/dumpit/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/dumpit/registration/RegistrationActivity.kt
@@ -1,12 +1,110 @@
 package com.example.dumpit.registration
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dumpit.R
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import androidx.appcompat.widget.AppCompatButton
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState:Bundle?) {
+
+    private lateinit var fullNameEditText: MaterialAutoCompleteTextView
+    private lateinit var addressEditText: MaterialAutoCompleteTextView
+    private lateinit var usernameEditText: MaterialAutoCompleteTextView
+    private lateinit var emailEditText: MaterialAutoCompleteTextView
+    private lateinit var passwordEditText: MaterialAutoCompleteTextView
+    private lateinit var confirmPasswordEditText: MaterialAutoCompleteTextView
+    private lateinit var genderSpinner: Spinner
+    private lateinit var registerButton: AppCompatButton
+    private lateinit var alreadyHasAccountTextView: TextView
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Initialize views
+        fullNameEditText = findViewById(R.id.fullNameEditText)
+        addressEditText = findViewById(R.id.addressEditText)
+        usernameEditText = findViewById(R.id.registerUsernameEditText)
+        emailEditText = findViewById(R.id.registerEmailEditText)
+        passwordEditText = findViewById(R.id.registerPasswordEditText)
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText)
+        genderSpinner = findViewById(R.id.spinnerGender)
+        registerButton = findViewById(R.id.registerButton)
+        alreadyHasAccountTextView = findViewById(R.id.alreadyHasAccountTextView)
+
+        // Set click listeners
+        registerButton.setOnClickListener {
+            val fullName = fullNameEditText.text.toString()
+            val address = addressEditText.text.toString()
+            val username = usernameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            val confirmPassword = confirmPasswordEditText.text.toString()
+            val gender = genderSpinner.selectedItem.toString()
+
+            // Validate user input (add your own validation logic)
+            if (isValidInput(fullName, address, username, email, password, confirmPassword, gender)) {
+                // Register the user using Firebase Authentication
+                registerUser(email, password)
+            } else {
+                showToast("Please fill in all the fields and make sure passwords match.")
+            }
+        }
+
+        alreadyHasAccountTextView.setOnClickListener {
+            // Navigate to the login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun isValidInput(
+        fullName: String,
+        address: String,
+        username: String,
+        email: String,
+        password: String,
+        confirmPassword: String,
+        gender: String
+    ): Boolean {
+        // Add your validation logic here
+        // For example, you might check if the fields are not empty and if passwords match
+        return fullName.isNotEmpty() &&
+                address.isNotEmpty() &&
+                username.isNotEmpty() &&
+                email.isNotEmpty() &&
+                password.isNotEmpty() &&
+                confirmPassword.isNotEmpty() &&
+                password == confirmPassword
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration success
+                    showToast("Registration successful!")
+                    // You can add additional logic here, such as navigating to the home screen
+                } else {
+                    // If registration fails, display a message to the user.
+                    showToast("Registration failed. Please try again.")
+                }
+            }
+    }
+
+    private fun showToast(message: String) {
+        // Function to show a toast message
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
