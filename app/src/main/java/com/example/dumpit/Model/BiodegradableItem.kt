dc8221747f217392642d19493436fb72e3e03d4a
Siddharth Yadav <ykunal422@gmail.com>
2023-11-29 16:22:28 +0530

    here we go

--- app/src/main/java/com/example/dumpit/Model/BiodegradableItem.kt
+++ app/src/main/java/com/example/dumpit/Model/BiodegradableItem.kt
@@ -1,3 +1,6 @@
 package com.example.dumpit.Model
 
 data class BiodegradableItem(val name: String, val pricePerKg: Double)
+data class nonBiodegradableItem(val name: String, val pricePerKg: Double)
+data class ChemicalItem(val name: String, val pricePerKg: Double)
+data class industrialitem(val name: String, val pricePerKg: Double)
