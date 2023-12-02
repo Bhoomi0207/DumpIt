5efef3f1223ef0dbee0cfc6d80a4e3024ecbb0af
Siddharth Yadav <ykunal422@gmail.com>
2023-12-02 14:21:58 +0530

    here we go

---
+++ app/src/main/java/com/example/dumpit/Model/NonBiodegradableItemDataSource.kt
@@ -0,0 +1,28 @@
+package com.example.dumpit.Model
+
+object NonBiodegradableItemDataSource {
+    val nonBiodegradableItems = listOf(
+        NonBiodegradableItem("Plastic Bottles", 1.5),
+        NonBiodegradableItem("Aluminum Cans", 2.0),
+        NonBiodegradableItem("Styrofoam Containers", 3.0),
+        NonBiodegradableItem("Metallic Packaging", 2.5),
+        NonBiodegradableItem("Plastic Cutlery", 1.0),
+        NonBiodegradableItem("Polyethylene Bags", 3.5),
+        NonBiodegradableItem("Glass Bottles", 2.0),
+        NonBiodegradableItem("Rubber Tires", 4.5),
+        NonBiodegradableItem("Electronics Waste", 5.0),
+        NonBiodegradableItem("Plastic Wrappers", 1.5),
+        NonBiodegradableItem("Disposable Diapers", 2.0),
+        NonBiodegradableItem("Synthetic Fabrics", 3.5),
+        NonBiodegradableItem("Metallic Straws", 2.0),
+        NonBiodegradableItem("Non-Recyclable Paper", 2.5),
+        NonBiodegradableItem("Nylon Ropes", 4.0),
+        NonBiodegradableItem("Chemical Containers", 3.0),
+        NonBiodegradableItem("Plastic Toys", 1.5),
+        NonBiodegradableItem("Composite Materials", 2.0),
+        NonBiodegradableItem("PVC Pipes", 3.5),
+        NonBiodegradableItem("Plastic Packaging Materials", 2.0),
+        NonBiodegradableItem("Non-Biodegradable Glitter", 5.0)
+        // Add more items as needed
+    )
+}
